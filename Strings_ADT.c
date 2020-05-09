43706f901366ff07ac048cb8f3c0ec89eb8c712a
gogocrimemaster <gogocrimemaster2002@gmail.com>
2020-05-09 15:35:36 +0530

    String ADT revamped

--- Strings_ADT.c
+++ Strings_ADT.c
@@ -1,125 +1,163 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #include<ctype.h>
 #define MAX_STRING_LIMIT 1024
 
 typedef enum{False,True} MyBool;
 
-int MyStringLen(char *ptr)
+typedef struct
+{
+    char *chr;
+    int length;
+}String;
+
+void getString(String *ptr)
+{
+    char temp[MAX_STRING_LIMIT];
+    int index =0;
+
+    fgets(temp,MAX_STRING_LIMIT,stdin);
+    int len = strlen(temp);
+
+    if (0 < len )
+    {
+        ptr->chr = (char *)malloc(len);
+    }
+    else if ((0 >len) || (NULL == ptr->chr))
+    {
+        fprintf(stderr,"ERROR:Memory Allocation Failed\n");
+        exit (1);
+    }
+
+    for (index =0;index<len;index++)
+    {
+        ptr->chr[index] = temp[index];
+    }
+    *(ptr->chr +len -1) = '\0';
+    ptr->length = strlen(ptr->chr);
+}
+
+char *DisplayString(String *ptr)
+{
+    if (0 > ptr->length)
+    {
+        fprintf(stderr,"ERROR:string length is 0 no string found ");
+        exit (1);
+    }
+    else
+        return ptr->chr;
+}
+
+
+int MyStringLen(String *ptr)
 {
     int i = 0;
-    char *temp = ptr;
-    while(*temp!= '\0')
+    while(ptr->chr[i]!= '\0')
     {
         i++;
-        temp++;
     }
     return i;
 }
 
-void Mytoupper(char *chr)
+void Mytoupper(String *ptr)
 {
-    char *temp = chr;
-    while(*temp !='\0')
+    int i = 0;
+    while(ptr->chr[i] !='\0')
     {
-        if ((*temp >= 97) && (*temp<=122))
+        if ((ptr->chr[i] >= 97) && (ptr->chr[i]<=122))
         {
-            *temp -=32;
+            ptr->chr[i] -=32;
         }
-        temp++;
+        i++;
     }
-     temp = chr;
-    fprintf(stdout," %s\n",temp);
+    fprintf(stdout," %s\n",DisplayString(ptr));
 }
 
-void Mytolower(char *chr)
-{
-    char *temp = chr;
-    while(*temp !='\0')
+void Mytolower(String *ptr)
+{ 
+    int i = 0;
+    while(ptr->chr[i] !='\0')
     {
-        if ((*temp >= 65) && (*temp<=90))
+        if ((ptr->chr[i] >= 65) && (ptr->chr[i]<=90))
         {
-            *temp +=32;
+            ptr->chr[i] +=32;
         }
-        temp++;
+        i++;
     }
-     temp = chr;
-    fprintf(stdout," %s\n",temp);
+    fprintf(stdout," %s\n",DisplayString(ptr));
 }
 
-void ToggleString(char *chr)
+void ToggleString(String *ptr)
 {
-    char *temp = chr;
-    while(*temp !='\0')
+    int i=0;
+    while(ptr->chr[i] !='\0')
     {
-        if ((*temp >= 65) && (*temp<=90))
+        if ((ptr->chr[i] >= 65) && (ptr->chr[i]<=90))
         {
-            *temp +=32;
+            ptr->chr[i] +=32;
         }
-        else if ((*temp >= 97) && (*temp<=122))
+        else if ((ptr->chr[i] >= 97) && (ptr->chr[i]<=122))
         {
-            *temp -=32;
+            ptr->chr[i] -=32;
         }
-
-        temp++;
+        i++;
     }
-     temp = chr;
-    fprintf(stdout," %s\n",temp);
+    fprintf(stdout," %s\n",DisplayString(ptr));
 }
 
-int word_count(char *ptr)
+int word_count(String *ptr)
 {
     int count = 0;
-    char *temp = ptr;
     int i =0;
-    while(temp[i] != '\0')
+    while(ptr->chr[i] != '\0')
     {
-        if ((temp[i] == ' ')&&(temp[i-1] != ' '))
+        if ((ptr->chr[i] == ' ')&&(ptr->chr[i-1] != ' '))
             count++;
         i++;
     }
     return count+1;
 }
 
-int sentence_count(char *ptr)
+int sentence_count(String *ptr)
 {
+    int i = 0;
     int count = 0;
-    char *temp = ptr;
-    while(*temp != '\0')
+    while(ptr->chr[i] != '\0')
     {
-        if (*temp == '.')
+        if (ptr->chr[i] == '.')
             count++;
-        temp++;
+        i++;
     }
     return count;
 }
 
-int para_count(char *ptr)
+int para_count(String *ptr)
 {
     int count = 0;
-    char *temp = ptr;
-    while(*temp != '\0')
+    int i = 0;
+    while(ptr->chr[i] != '\0')
     {
-        if (*temp == '\n')
+        if (ptr->chr[i] == '\n')
             count++;
-        temp++;
+        i++;
     }
     return count;
 }
 
-int isAlphaNumereic(char *ptr)
+int isAlphaNumereic(String *ptr)
 {
-    char *temp = ptr;
     MyBool NumericFound = False;
     int count = 0;
-    while(*temp != '\0')
+    int i = 0;
+    while(ptr->chr[i] != '\0')
     {
-        if ((*temp >= 48) && (*temp <= 57))
+        if ((ptr->chr[i] >= 48) && (ptr->chr[i] <= 57))
         {
             NumericFound = True;
             count++;
         }
-        temp++;
+        i++;
     }
     if(False != NumericFound)
     {
@@ -129,15 +167,16 @@ int isAlphaNumereic(char *ptr)
     return 0;
 }
 
-int isVowel(char *ptr)
+int isVowel(String *ptr)
 {
-    char *temp = ptr;
     int count  = 0;
-    while(*temp !='\0')
+    int i=0;
+    while(ptr->chr[i] !='\0')
     {
-        if ((*temp >= 65) && (*temp<=90) || ((*temp >= 97) && (*temp<=122)))
+        if ((ptr->chr[i] >= 65) && (ptr->chr[i]<=90) ||
+                ((ptr->chr[i] >= 97) && (ptr->chr[i]<=122)))
         {
-            switch(*temp)
+            switch(ptr->chr[i])
             {
                 case 'a':
                 case 'e':
@@ -154,7 +193,7 @@ int isVowel(char *ptr)
                          break;
             }
         }
-        temp++;
+        i++;
     }
     if(0 != count)
     {
@@ -163,15 +202,16 @@ int isVowel(char *ptr)
     return 0;
 }
 
-int isConsonent(char *ptr)
+int isConsonent(String *ptr)
 {
-    char *temp = ptr;
     int count  = 0;
-    while(*temp !='\0')
+    int i=0;
+    while(ptr->chr[i] !='\0')
     {
-        if ((*temp >= 65) && (*temp<=90) || ((*temp >= 97) && (*temp<=122)))
+        if ((ptr->chr[i] >= 65) && (ptr->chr[i]<=90) ||
+                ((ptr->chr[i] >= 97) && (ptr->chr[i] <= 122)))
         {
-            switch(*temp)
+            switch(ptr->chr[i])
             {
                 case 'a':
                 case 'e':
@@ -188,7 +228,7 @@ int isConsonent(char *ptr)
                 count++;
             }
         }
-        temp++;
+        i++;
     }
     if(0 != count)
     {
@@ -197,76 +237,108 @@ int isConsonent(char *ptr)
     return 0;
 }
 
-char *MyStringRev(char *ptr)
+char *MyStringRev(String *ptr)
 {
-    char *temp = ptr,aux;
-    int i=0,len=0;
-    len = MyStringLen(ptr)-1;
-    while(*temp != '\0'){
-        temp++;
-    }
-    temp--;
+    int len=0,i=0;
+    char aux;
+    len = MyStringLen(ptr);
 
-    //while(*ptr=*temp && *temp-- !='\0' && *ptr++ !='\0');
-    i = 0;
-    if(len%2==0)
+    for (i = 0 ; i<len/2;i++)
     {
-        while(i < len/2)
-        {
-            aux = *temp;
-            *temp = *ptr;
-            *ptr = aux;
-            i++;
-            temp--;ptr++;
-        }
-        ptr -= (len/2)-1; 
+        aux = ptr->chr[len-i];
+        ptr->chr[len-i] = ptr->chr[i];
+        ptr->chr[i] = aux;
     }
+
+    return ptr->chr;
+}
+
+int MyStringcmp(String *str1,String *str2)
+{
+    int i =0;
+    MyBool isequal = True;
+    if (str1->length != str2->length)
+        isequal = False;
     else
     {
-        while(i < (len/2)+1)
+        for(i = 0 ;i<str1->length;i++)
         {
-            aux = *temp;
-            *temp = *ptr;
-            *ptr = aux;
-            i++;
-            temp--;ptr++;
+            if (str1->chr[i] != str2->chr[i])
+            {
+                isequal = False;
+                break;
+            }
         }
-        ptr -= (len/2); 
     }
+    if (False == isequal)
+    {
+        return 1;
+    }
+    return 0;
+}
 
-
-    return ptr;
+int MyStringcmpi(String *str1,String *str2)
+{
+    int i =0;
+    MyBool isequal = True;
+    if (str1->length != str2->length)
+        isequal = False;
+    else
+    {
+        Mytolower(str1);
+        Mytolower(str2);
+        for(i = 0 ;i<str1->length;i++)
+        {
+            if (str1->chr[i] != str2->chr[i])
+            {
+                isequal = False;
+                break;
+            }
+        }
+    }
+    if (False == isequal)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 int main()
 {
-    char temp[MAX_STRING_LIMIT];
-    char *string; 
-    int choice = 0;
-    int cont = 0;
+    String *string     = NULL; 
+    unsigned char cont = '0';
+    int choice         = 0;
+
+    string = (String *)malloc(sizeof(String));
+    if (NULL == string)
+    {
+        fprintf(stderr,"ERROR:Memory Allocation Failed\n");
+        exit (1);
+    }
+    memset(string,0,sizeof(String));
 
     fprintf(stdout,"Enter the String :");
-    fgets(temp,MAX_STRING_LIMIT,stdin);
 
-    string = temp;
+    getString(string);
 
-    fprintf(stdout,"Entered  String : %s\n",string);
-    
+    fprintf(stdout,"Entered  String : %s\n",DisplayString(string));
+
     do
     {
-        printf("Menue Option :\n");
-        printf("1.String Length\n2.Lower to Upper case\n3.Upper to Lower\n4.Size of\n");
-        printf("5.Toggle case\n6.No. of words,sentence,paragraph\n");
-        printf("7.Is alpha numeric\n8.Vowel and Vowel count\n9.Consonent and consonent count\n");
-        printf("10.String Reverse\n");
-        printf("Enter choice:");
+        fprintf(stdout,"Menue Option :\n");
+        fprintf(stdout,"1.String Length\n2.Lower to Upper case\n3.Upper to Lower\n4.Size of\n");
+        fprintf(stdout,"5.Toggle case\n6.No. of words,sentence,paragraph\n");
+        fprintf(stdout,"7.Is alpha numeric\n8.Vowel and Vowel count\n9.Consonent and consonent count\n");
+        fprintf(stdout,"10.String Reverse\n11.Comparing to string\n");
+        fprintf(stdout,"Enter choice:");
         scanf("%d",&choice);
+        getchar();
         switch(choice)
         {
             case 1 :
             {
                 fprintf(stdout,
-                        "String Length using Library function : %ld\n",strlen(string));
+                        "String Length using Library function : %ld\n",strlen(string->chr));
 
                 fprintf(stdout,
                         "String Length using MyStringLen : %d\n",MyStringLen(string));
@@ -275,13 +347,12 @@ int main()
             case 2:
             {
                 int i = 0;
-                char *temp = string;
                 fprintf(stdout,
                         "Change Lower to Upper using Library function:");
 
-                while(i <MyStringLen(temp))
+                while(i <MyStringLen(string))
                 {
-                    printf("%c",toupper(temp[i]));
+                    printf("%c",toupper(string->chr[i]));
                     i++;
                 }
                 printf("\n");
@@ -296,13 +367,12 @@ int main()
             case 3:
             {
                 int i = 0;
-                char *temp = string;
                 fprintf(stdout,
                         "Change upper to lower using Library function:");
 
-                while(i <MyStringLen(temp))
+                while(i <MyStringLen(string))
                 {
-                    printf("%c",tolower(temp[i]));
+                    printf("%c",tolower(string->chr[i]));
                     i++;
                 }
 
@@ -360,7 +430,7 @@ int main()
             }
             case 9:
             {
-                 if(0 == isConsonent(string))
+                if(0 == isConsonent(string))
                 {
                     printf("No consonent present\n");
                 }
@@ -373,15 +443,78 @@ int main()
             }
             case 10:
             {
-
                 printf("Reverse String :%s\n",MyStringRev(string));
-
                 break;
+            }
+            case 11:
+            {
+                String *string2 = NULL;
+                string2 = (String *)malloc(sizeof(String));
+                if (NULL == string2)
+                {
+                    fprintf(stderr,"ERROR:Memory Allocation Failed\n");
+                    exit (1);
+                }
+                memset(string2,0,sizeof(String));
+
+                fprintf(stdout,"Enter String to compare :");
+                getString(string2);
+
+                fprintf(stdout,"Entered  String1 : %s\n",DisplayString(string2));
+
+                fprintf(stdout,"String compare using Library function \n");
+                if (strcmp(string->chr,string2->chr)==0)
+                {
+                    fprintf(stdout,"String %s and %s are equal \n",DisplayString(string),DisplayString(string2));
+                }
+                else
+                {
+                    fprintf(stdout,"String %s and %s are not equal \n",DisplayString(string),DisplayString(string2));
+                }
+
+                fprintf(stdout,"String compare using MyStringcmp \n");
+                if (MyStringcmp(string,string2)==0)
+                {
+                    fprintf(stdout,"String %s and %s are equal \n",DisplayString(string),DisplayString(string2));
+                }
+                else
+                {
+                    fprintf(stdout,"String %s and %s are not equal \n",DisplayString(string),DisplayString(string2));
+                }
+
+                fprintf(stdout,"String compare(ignore case) using MyStringcmpi \n");
+                if (MyStringcmpi(string,string2)==0)
+                {
+                    fprintf(stdout,"String %s and %s are not equal \n",DisplayString(string),DisplayString(string2));
+                }
+                else
+                {
+                    fprintf(stdout,"String %s and %s are not  equal \n",DisplayString(string),DisplayString(string2));
+                }
+
+                if ((NULL != string2->chr) &&
+                        (NULL != string2))
+                {
+                    free(string2->chr);
+                    string2->chr = NULL;
+                    free(string2);
+                    string2 = NULL;
+                }
+                break; 
             }
             default: printf("Wrong Choice\n");
         }
         printf("Do you want to continue (1/0): ");
-        scanf("%d",&cont);
-    }while(cont== 1 );
+        scanf("%c",&cont);
+    }while(cont== '1' );
+
+    if ((NULL != string->chr) &&
+            (NULL != string))
+    {
+        free(string->chr);
+        string->chr = NULL;
+        free(string);
+        string = NULL;
+    }
     return 0;
 }
